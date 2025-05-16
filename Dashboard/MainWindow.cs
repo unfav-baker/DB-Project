595e09acbd77fd9e3bcbdc3bf4a366c9e530fc7e
unfav-baker <abubakermukhtar55@gmail.com>
2025-05-16 12:50:22 +0500

    Admin Frontend with COnnectivity

---
+++ Dashboard/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Dashboard
+{
+    internal class MainWindow
+    {
+        public WindowState WindowState { get; set; }
+        public double Left { get; internal set; }
+        public double Width { get; internal set; }
+        public double Top { get; internal set; }
+        public double Height { get; internal set; }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
