bb92f74ee998ed07309c1e99affd62ce051b4562
unfav-baker <abubakermukhtar55@gmail.com>
2025-05-17 17:24:19 +0500

    Adding Connected Frontend of Employee

---
+++ Generate Reports.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Employee
+{
+    /// <summary>
+    /// Interaction logic for Generate_Reports.xaml
+    /// </summary>
+    public partial class Generate_Reports : Page
+    {
+        public Generate_Reports()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LGE.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Employee
+{
+    /// <summary>
+    /// Interaction logic for LGE.xaml
+    /// </summary>
+    public partial class LGE : Window
+    {
+        public LGE()
+        {
+            InitializeComponent();
+        }
+        private void CloseApplication_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();// Optional: close current window
+        }
+
+
+    }
+
+}
