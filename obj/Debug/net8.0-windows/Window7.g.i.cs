ca86acd2f9f149b4257e6953004bf11303c71d34
unfav-baker <abubakermukhtar55@gmail.com>
2025-05-22 16:50:55 +0500

    Some Changes

--- obj/Debug/net8.0-windows/Window7.g.i.cs
+++ obj/Debug/net8.0-windows/Window7.g.i.cs
@@ -54,7 +54,7 @@ namespace Dashboard {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dashboard;V1.0.0.0;component/window7.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dashboard;component/window7.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window7.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
